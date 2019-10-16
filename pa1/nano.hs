65e38a430cf48d79d5c78337a12dadc59565bad4
Daniel Xiong <dxiong2000@gmail.com>
2019-10-16 15:36:09 -0700

    updated nano 10/16/19

--- pa1/nano.hs
+++ pa1/nano.hs
@@ -18,10 +18,10 @@ data Expr = Constant Float |
 	 NotEquals Expr Expr deriving (Show) 
 
 data Stmt =
-     Let String Expr |
+     Let Expr Expr |
      Print [Expr] |
      IfGoto Expr String |
-     Input String deriving (Show) 
+     Input Expr deriving (Show) 
 
 -- dummy predicate that is supposed to check if a string is a label which is a string ending with ":"
 isLabel :: String -> Bool
@@ -45,9 +45,9 @@ parseExpr [x] = if (isAlpha (head x)) then (Var x) else (Constant (read x)) -- p
 -- takes the first token which should be a keyword and a list of the remaining tokens and returns the parsed Stmt
 parseStmt :: String -> [String] -> Stmt
 parseStmt "let" (v:"=":expr) = Let v (parseExpr expr)
-parseStmt "print" expr = Print (parseExpr expr)
-parseStmt "if" expr1 "goto" label = IfGoto (parseExpr expr1) (label)
-parseStmt "input" varName = Input varName
+parseStmt "print" (expr) = Print (parseExpr expr)
+parseStmt "if" (expr, "goto", label) = IfGoto (parseExpr expr) label
+parseStmt "input" varName = Input (parseExpr varName)
 
 -- takes a list of tokens and returns the parsed statement - the statement may include a leading label
 parseLine :: [String] -> Stmt
@@ -65,12 +65,21 @@ eval :: Expr -> SymTable -> Float
 eval (Var v) env = lookupVar v env
 eval (Constant v) _ = v
 eval (Plus e1 e2) env = (eval e1 env) + (eval e2 env)
+eval (Minus e1 e2) env = (eval e1 env) - (eval e2 env)
+eval (Mult e1 e2) env = (eval e1 env) * (eval e2 env)
+eval (Divide e1 e2) env = (eval e1 env) / (eval e2 env)
+eval (LessThan e1 e2) env = if (eval e1 env) < (eval e2 env) then True else False
+eval (GreaterThan e1 e2) env = if (eval e1 env) > (eval e2 env) then True else False
+eval (LessThanEQ e1 e2) env = if (eval e1 env) <= (eval e2 env) then True else False
+eval (GreaterThanEQ e1 e2) env = if (eval e1 env) >= (eval e2 env) then True else False
+eval (Equals e1 e2) env = if (eval e1 env) == (eval e2 env) then True else False
+eval (NotEquals e1 e2) env = if (eval e1 env) != (eval e2 env) then True else False
 
 -- given a statement, a ST, line number, input and previous output, return an updated ST, input, output, and line number
 -- this starter version ignores the input and line number
 -- Stmt, SymTable, progCounter, input, output, (SymTable', input', output', progCounter)
 perform:: Stmt -> SymTable -> Float -> [String] ->String -> (SymTable, [String], String, Float)
-perform (Print e) env lineNum input output = (env, input, output++(show (eval e env)++"\n"), lineNum+1)
+perform (Print e) env lineNum input output = (env, input, output++(show (eval e env)++"\n"), lineNum+1) -- Print e now takes e as [String], so find a way to make it print the list
 perform (Let id e) env lineNum input output = ((id,(eval e env)):env, input, output, lineNum+1)
 
 -- given a list of Stmts, a ST, and current output, perform all of the statements in the list and return the updated output String
