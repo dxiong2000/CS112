4e07fff4e475082e0c463871f1ed59adf49b8fc0
Daniel Xiong <dxiong2000@gmail.com>
2019-11-03 22:33:13 -0800

    updated

--- pa2/prog2.pl
+++ pa2/prog2.pl
@@ -1,3 +1,4 @@
+
 print_trip( Action, Code, Name, time( Hour, Minute)) :-
    upcase_atom( Code, Upper_code),
    format( "~6s  ~3s  ~s~26|  ~02d:~02d",
@@ -8,6 +9,92 @@ test :-
    print_trip( depart, nyc, 'New York City', time( 9, 3)),
    print_trip( arrive, lax, 'Los Angeles', time( 14, 22)).
 
-doSomething(nyc,lax) :- test.
 
-main :- read(A),read(B), doSomething(A,B).
+degToRad(degmin(Deg, Min), Rad) :- 
+	Rad is ((pi*(Deg + (Min / 60))) / 180).
+
+% Lat, Long are of type degmin(D,M)
+latAndLong(Lat, Long, Lat1, Long1) :- 
+	degToRad(Lat, Lat1), 
+	degToRad(Long, Long1).
+
+% LatA, LongA, LatB, LongB are of type degmin(D,M)
+getDist(LatA, LongA, LatB, LongB, Dist) :-
+	latAndLong(LatA, LongA, Lat1, Long1),
+	latAndLong(LatB, LongB, Lat2, Long2),
+	Dlat is Lat2-Lat1,
+	Dlong is Long2-Long1,
+	A is (sin(Dlat / 2))^2 + (cos(Lat1) * cos(Lat2) * (sin(Dlong / 2))^2),
+	C is 2 * atan2(sqrt(A), sqrt(1-A)),
+	Dist is 3956 * C.
+
+% A, B are airport codes, return value Time is time in hours to get from A to B
+travelTime(A, B, Time) :- airport(A, _, LatA, LongA), airport(B, _, LatB, LongB), getDist(LatA, LongA, LatB, LongB, Dist), Time is (Dist / 500).
+
+fly(A, B) :- flight(A, B, _).
+fly(A, B) :- 
+	flight(A, L, _),
+	!,
+	fly(L, B).
+
+
+main :- read(A),read(B), fly(A, B).
+
+
+% database
+airport( atl, 'Atlanta         ', degmin(  33,39 ), degmin(  84,25 ) ).
+airport( bos, 'Boston-Logan    ', degmin(  42,22 ), degmin(  71, 2 ) ).
+airport( chi, 'Chicago         ', degmin(  42, 0 ), degmin(  87,53 ) ).
+airport( den, 'Denver-Stapleton', degmin(  39,45 ), degmin( 104,52 ) ).
+airport( dfw, 'Dallas-Ft.Worth ', degmin(  32,54 ), degmin(  97, 2 ) ).
+airport( lax, 'Los Angeles     ', degmin(  33,57 ), degmin( 118,24 ) ).
+airport( mia, 'Miami           ', degmin(  25,49 ), degmin(  80,17 ) ).
+airport( nyc, 'New York City   ', degmin(  40,46 ), degmin(  73,59 ) ).
+airport( sea, 'Seattle-Tacoma  ', degmin(  47,27 ), degmin( 122,17 ) ).
+airport( sfo, 'San Francisco   ', degmin(  37,37 ), degmin( 122,23 ) ).
+airport( sjc, 'San Jose        ', degmin(  37,22 ), degmin( 121,56 ) ).
+
+flight( bos, nyc, time( 7,30 ) ).
+flight( dfw, den, time( 8, 0 ) ).
+flight( atl, lax, time( 8,30 ) ).
+flight( chi, den, time( 8,45 ) ).
+flight( mia, atl, time( 9, 0 ) ).
+flight( sfo, lax, time( 9, 0 ) ).
+flight( sea, den, time( 10, 0 ) ).
+flight( nyc, chi, time( 11, 0 ) ).
+flight( sea, lax, time( 11, 0 ) ).
+flight( den, dfw, time( 11,15 ) ).
+flight( sjc, lax, time( 11,15 ) ).
+flight( atl, lax, time( 11,30 ) ).
+flight( atl, mia, time( 11,30 ) ).
+flight( chi, nyc, time( 12, 0 ) ).
+flight( lax, atl, time( 12, 0 ) ).
+flight( lax, sfo, time( 12, 0 ) ).
+flight( lax, sjc, time( 12, 15 ) ).
+flight( nyc, bos, time( 12,15 ) ).
+flight( bos, nyc, time( 12,30 ) ).
+flight( den, chi, time( 12,30 ) ).
+flight( dfw, den, time( 12,30 ) ).
+flight( mia, atl, time( 13, 0 ) ).
+flight( sjc, lax, time( 13,15 ) ).
+flight( lax, sea, time( 13,30 ) ).
+flight( chi, den, time( 14, 0 ) ).
+flight( lax, nyc, time( 14, 0 ) ).
+flight( sfo, lax, time( 14, 0 ) ).
+flight( atl, lax, time( 14,30 ) ).
+flight( lax, atl, time( 15, 0 ) ).
+flight( nyc, chi, time( 15, 0 ) ).
+flight( nyc, lax, time( 15, 0 ) ).
+flight( den, dfw, time( 15,15 ) ).
+flight( lax, sjc, time( 15,30 ) ).
+flight( chi, nyc, time( 18, 0 ) ).
+flight( lax, atl, time( 18, 0 ) ).
+flight( lax, sfo, time( 18, 0 ) ).
+flight( nyc, bos, time( 18, 0 ) ).
+flight( sfo, lax, time( 18, 0 ) ).
+flight( sjc, lax, time( 18,15 ) ).
+flight( atl, mia, time( 18,30 ) ).
+flight( den, chi, time( 18,30 ) ).
+flight( lax, sjc, time( 19,30 ) ).
+flight( lax, sfo, time( 20, 0 ) ).
+flight( lax, sea, time( 22,30 ) ).
