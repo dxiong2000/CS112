4614e096e618e770c99fc1ef40114db4e7354e67
Daniel Xiong <dxiong2000@gmail.com>
2019-12-02 15:07:34 -0800

    done. TODO: test edge cases

--- pa4/TLI.scala
+++ pa4/TLI.scala
@@ -1,6 +1,8 @@
 import scala.collection.mutable.Map
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.StringBuilder
 import scala.io.Source
+import scala.io.StdIn.readLine
 import util.control.Breaks._
 
 abstract class Expr
@@ -17,26 +19,60 @@ case class Print(exprList: ArrayBuffer[Expr]) extends Stmt
 case class No_Op() extends Stmt
 
 object TLI {
-    def eval(expr: Expr, symTable: Map[String, Double]): Double = expr match {
-        case BinOp("+",e1,e2) => eval(e1,symTable) + eval(e2,symTable) 
-        case BinOp("-",e1,e2) => eval(e1,symTable) - eval(e2,symTable) 
-        case BinOp("*",e1,e2) => eval(e1,symTable) * eval(e2,symTable) 
-        case BinOp("/",e1,e2) => eval(e1,symTable) / eval(e2,symTable) 
-        case BinOp("<",e1,e2) => {
-            if (eval(e1,symTable) < eval(e2,symTable)){
-                1
+    def eval(expr: Expr, symTable: Map[String, Double], lineNum: Int): Option[Either[String, Double]] = expr match {
+        case BinOp("+",e1,e2) => Some(Right(eval(e1,symTable,lineNum).get.right.get + eval(e2,symTable,lineNum).get.right.get))
+        case BinOp("-",e1,e2) => Some(Right(eval(e1,symTable,lineNum).get.right.get - eval(e2,symTable,lineNum).get.right.get))
+        case BinOp("*",e1,e2) => Some(Right(eval(e1,symTable,lineNum).get.right.get * eval(e2,symTable,lineNum).get.right.get))
+        case BinOp("/",e1,e2) => Some(Right(eval(e1,symTable,lineNum).get.right.get / eval(e2,symTable,lineNum).get.right.get))
+        case BinOp("<",e1,e2) =>
+            if (eval(e1,symTable,lineNum).get.right.get < eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
             }
             else{
-                0
+                return Some(Right(0))
             }
-        }
-        case BinOp(">",e1,e2) => eval(e1,symTable) - eval(e2,symTable) 
-        case Var(name) => symTable(name)
-        case Constant(num) => num
-	    case _ => {
-            println("error")
-            0
-        } // should really throw an error
+        case BinOp(">",e1,e2) => 
+            if (eval(e1,symTable,lineNum).get.right.get > eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
+            }
+            else{
+                return Some(Right(0))
+            }
+        case BinOp("<=", e1, e2) =>
+            if (eval(e1,symTable,lineNum).get.right.get <= eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
+            }
+            else{
+                return Some(Right(0))
+            }
+        case BinOp(">=", e1, e2) =>
+            if (eval(e1,symTable,lineNum).get.right.get >= eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
+            }
+            else{
+                return Some(Right(0))
+            }
+        case BinOp("==", e1, e2) =>
+            if (eval(e1,symTable,lineNum).get.right.get == eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
+            }
+            else{
+                return Some(Right(0))
+            }
+        case BinOp("!=", e1, e2) =>
+            if (eval(e1,symTable,lineNum).get.right.get != eval(e2,symTable,lineNum).get.right.get){
+                return Some(Right(1))
+            }
+            else{
+                return Some(Right(0))
+            }
+        case Var(name) => Some(Right(symTable(name)))
+        case Constant(num) => Some(Right(num))
+        case PrintStr(name) => Some(Left(name))
+	    case _ => 
+            println(s"Syntax error on line $lineNum.")
+            System.exit(0)
+            None
     }
 
     def parseLine(lines: ArrayBuffer[Array[String]], stmtList: ArrayBuffer[Stmt], symTable: Map[String, Double]): (ArrayBuffer[Stmt], Map[String, Double]) = {
@@ -129,7 +165,7 @@ object TLI {
             return Some(Print(exprList))
         case Array("if", op1, operator, op2, "goto", label) => // If(expr: Expr, label: String) 
             var expr = parseExpr(Array(op1,operator,op2), lineNum).get
-            return Some(If(expr, label))
+            return Some(If(expr, label+":"))
         case Array("if", op, "goto", label) => // If(expr: Expr, label: String) 
             var expr = parseExpr(Array(op), lineNum).get
             return Some(If(expr, label))
@@ -198,6 +234,58 @@ object TLI {
             None
     }
 
+    def perform(stmt: Stmt, output: StringBuilder, symTable: Map[String, Double], lineNum: Int): Option[(StringBuilder, Map[String, Double], Int)] = stmt match{
+        case Let(v, expr) => // Let(variable: String, expr: Expr)
+            symTable(v) = eval(expr, symTable, lineNum).get.right.get
+            return Some((output, symTable, lineNum+1))
+        case Print(exprList) => // Print(exprList: ArrayBuffer[Expr])
+            for ((expr, i) <- exprList.zipWithIndex){
+                if (i == exprList.length-1){
+                    var evalOut = eval(expr, symTable, lineNum).get
+                    evalOut match {
+                        case Left(x) => output.append(x + "\n")
+                        case Right(x) => output.append(x.toString + "\n")
+                    }
+                }
+                else{
+                    var evalOut = eval(expr, symTable, lineNum).get
+                    evalOut match {
+                        case Left(x) => output.append(x + " ")
+                        case Right(x) => output.append(x.toString + " ")
+                    }
+                }
+            }
+            return Some((output, symTable, lineNum+1))
+        case If(expr, label) => // If(expr: Expr, label: String)
+            if (eval(expr, symTable, lineNum).get.right.get != 0){
+                if (symTable.contains(label)){
+                    return Some((output, symTable, symTable(label).toInt))
+                }
+                else{
+                    println(s"Illegal goto $label at line $lineNum.")
+                    System.exit(0)
+                    return None
+                }
+            }
+            else{
+                return Some((output, symTable, lineNum+1))
+            }
+        case Input(v) => // Input(variable: String)
+            try{
+                var input = readLine().toDouble
+                symTable(v) = input
+                return Some((output, symTable, lineNum+1))
+            }
+            catch{
+                case _ : Throwable =>
+                    println("Illegal or missing input.")
+                    System.exit(0)
+                    return None
+            }
+        case No_Op() =>
+            return Some((output, symTable, lineNum+1))
+    }
+
     def main(args: Array[String]) {
 
         // parses input file
@@ -213,18 +301,19 @@ object TLI {
         }
 
         // passes empty ArrayBuffer and empty Map into parseLine
-        // parseLine returns a 2-tuple of stmtList and symTable.
+        // parseLine returns a 2-tuple of stmtList: ArrayBuffer[Stmt] and symTable: Map[String, Double].
         var (stmtList, symTable) = parseLine(contents, new ArrayBuffer[Stmt](), scala.collection.mutable.Map[String, Double]())
 
-        println(stmtList)
-        println(symTable)
+        var lineNum: Int = 1
+        var output: StringBuilder = new StringBuilder()
+        stmtList.prepend(No_Op()) // pad for 1-indexing
+        while (lineNum < stmtList.length){
+            var (output_, symTable_, lineNum_) = perform(stmtList(lineNum), output, symTable, lineNum).get
+            output = output_
+            symTable = symTable_
+            lineNum = lineNum_
+        }
 
-        // for (line <- contents){
-        //     println(line.length)
-        //     for (word <- line){
-        //         print(word + " ")
-        //     }
-        //     println()
-        // }
+        print(output)
     }
 }
